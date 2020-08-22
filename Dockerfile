f0c414fd79dfdb5a51fcafbff7ab59770b19cb0e
Zaiming Shi <zmstone@gmail.com>
2020-08-22 20:58:26 +0200

    Base on alpine

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
-FROM ubuntu:14.04
+FROM erlang:23-alpine as builder
+ARG GIT_TAG=master
 
-RUN apt-get update && apt-get install -y libssl-dev
-COPY brod/_build/brod_cli/rel/brod /brod
+RUN apk update && apk upgrade && \
+    apk add --no-cache bash git g++ make
+RUN git clone -b ${GIT_TAG} https://github.com/klarna/brod.git /brod
+WORKDIR /brod
+RUN make brod-cli
 
-ENV PATH /brod/bin:$PATH
-
+FROM alpine:3
+RUN apk update && apk upgrade && \
+    apk add --no-cache ncurses-dev openssl-dev libstdc++
+COPY --from=builder /brod/_build/brod_cli/rel/brod /brod
+ENV PATH=$PATH:/brod/bin
--- build-and-push.sh
+++ build-and-push.sh
@@ -1,24 +1,24 @@
-#!/bin/bash -eu
+#!/bin/bash
+
+set -euo pipefail
 
 if [ ! -d brod ]; then
   git clone https://github.com/klarna/brod.git
 fi
 cd brod
 GIT_DESC=$(git describe --tag)
-VSN=$(echo $GIT_DESC | grep -E '^[0-9]*\.[0-9]*\.[0-9]*$' || true)
-if [ -z $VSN ]; then
+VSN="$(echo "$GIT_DESC" | grep -E '^[0-9]*\.[0-9]*\.[0-9]*$' || true)"
+if [ -z "$VSN" ]; then
   echo "Not building $GIT_DESC"
   exit 0
 fi
-echo "building $VSN"
-make brod-cli
 cd ..
 
 LIB=zmstone
 
-docker build --build-arg BROD_VSN=$VSN -t $LIB/brod:$VSN .
-docker tag $LIB/brod:$VSN $LIB/brod:latest
+docker build --build-arg GIT_TAG="${GIT_DESC}" -t "$LIB/brod:$VSN" .
+docker tag "$LIB/brod:$VSN" "$LIB/brod:latest"
 
-docker push $LIB/brod:$VSN
-docker push $LIB/brod:latest
+docker push "$LIB/brod:$VSN"
+docker push "$LIB/brod:latest"
 
